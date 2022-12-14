0943ca86e30bed0dbf5bf0846fda5cd2bc08d975
rakabgs27 <rakabgs2000@gmail.com>
2022-12-14 08:00:16 +0700

    Fix Part 2

--- database/migrations/2022_03_31_045102_create_kategoris_table.php
+++ database/migrations/2022_03_31_045102_create_kategoris_table.php
@@ -13,7 +13,7 @@ class CreateKategorisTable extends Migration
      */
     public function up()
     {
-        Schema::create('Kategoris', function (Blueprint $table) {
+        Schema::create('kategoris', function (Blueprint $table) {
             $table->id();
             $table->string('nama_kategori');
             $table->softDeletes();
